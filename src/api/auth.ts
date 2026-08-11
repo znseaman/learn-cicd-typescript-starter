6c58d1837a4c81ffd935e1f1c5f2ce083e8a5610
znseaman <znseaman@gmail.com>
2026-08-11 12:37:13 -0700

    test: purposefully break test to verify ci failure

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -7,7 +7,7 @@ export function getAPIKey(headers: IncomingHttpHeaders): string | null {
   }
 
   const splitAuth = authHeader.split(" ");
-  if (splitAuth.length < 2 || splitAuth[0] !== "ApiKey") {
+  if (splitAuth.length < 2 || splitAuth[0] !== "apiKey") {
     return null;
   }
 
